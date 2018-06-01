d814aebfb4ca5c65fa277252896af53ebc356f4b
kmasakura <masakura@MacBook-Pro.local>
2018-06-01 16:04:05 -0700

    Basic structure

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -4,17 +4,51 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 public class InnReservations {
 	public static void main(String[] args) {
-		String dburl = "";
-		String dbuser = "";
-		String dbpass = "";
+		String dburl = "jdbc:mysql://" + args[0] + "?autoReconnect=true&verifyServerCertificate=false&useSSL=true";
+		String dbuser = args[1];
+		String dbpass = args[2];
 		try {
 			Connection c = DriverManager.getConnection(dburl, dbuser, dbpass);
+			System.out.println("Connected!");
+			prompt();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static void prompt() {
+		System.out.println("1: Rooms and Rates");
+		System.out.println("2: Reservations");
+		System.out.println("3: Reservation Change");
+		System.out.println("4: Reservation Cancellation");
+		System.out.println("5: Detailed Reservation Information");
+		System.out.println("6: Revenue");
+		System.out.print("Enter #:");
+		Scanner s = new Scanner(System.in);
+		if (s.hasNextInt()) {
+			int choice = s.nextInt();
+			switch (choice) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			default:
+				break;
+			}
+		}
+		s.close();
+		prompt();
 	}
 }
